ef3bec2c6d20bb18c113d9275d778b8b1b32f1ce
rcolepeterson <Cole.Peterson@possible.com>
2015-10-28 13:28:01 -0700

    top menu socila btns

--- app/scripts/modules/core/controllers/header.controller.js
+++ app/scripts/modules/core/controllers/header.controller.js
@@ -6,7 +6,7 @@ angular.module('core').controller('HeaderController', ['$scope', '$rootScope', '
     $scope.headerModel = {};
     //hide top menu > categories if we are not at home.
     $scope.doHideCategories = false;
-    
+
     /**
      * Listen for site wide dispatch that we have loaded the categories. define the categories on scope for top menu binding..
      */
@@ -16,16 +16,16 @@ angular.module('core').controller('HeaderController', ['$scope', '$rootScope', '
     });
 
     //turn off topmenu cats if we are not @home.
-    $rootScope.$on('$stateChangeStart',
-      function(event, toState, toParams, fromState, fromParams) {
-        if ( toState.name === "about" )
-        {
-          $scope.doHideCategories = true;
-        }else{
-
-          $scope.doHideCategories = false;
-        }
-      })
+    // $rootScope.$on('$stateChangeStart',
+    //   function(event, toState, toParams, fromState, fromParams) {
+    //     if ( toState.name === "about" )
+    //     {
+    //       $scope.doHideCategories = true;
+    //     }else{
+    //
+    //       $scope.doHideCategories = false;
+    //     }
+    //   })
 
     //user has selected a cat. broad cast event.
     $scope.selectACategory = function(n) {
